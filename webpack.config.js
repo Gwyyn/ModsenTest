dbbf9adda41323b767bbd2e778b7c67de5ed2d86
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-06-19 15:39:23 +0300

    correct devtool

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    devtool: 'inline-source-Map',
+    devtool: 'inline-source-map',
     devServer: {
         port: 3000,
         hot: true
