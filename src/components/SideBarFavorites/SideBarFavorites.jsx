bdf18d047df4a4021c175dec0f5769ddde6eedd0
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-06-22 23:23:19 +0300

    update main

--- src/components/SideBarFavorites/SideBarFavorites.jsx
+++ src/components/SideBarFavorites/SideBarFavorites.jsx
@@ -8,6 +8,7 @@ const SideBarFavorites = ({onToggleFavoritesSideBar}) => {
     return (
         <div className="col-auto col-lg-2 col-md-3 col-sm-3 col-3 d-flex flex-row position-relative">
             <div className="w-100 p-3">
+
             </div>
             <div className="d-flex align-items-center h-auto position-absolute top-50 start-100 ">
                 <div className={cl.toggleSideBarBtn} onClick={onToggleFavoritesSideBar}>
